e0c34692db38e16d50f99e9ed1867513c2de0841
LikeGhost <1154038659@qq.com>
2022-11-04 18:38:52 +0800

    [编译代码](develop-category-list-tree): 优化pom文件，将renren-fast集成进nacos

--- renren-fast/src/main/java/io/renren/RenrenApplication.java
+++ renren-fast/src/main/java/io/renren/RenrenApplication.java
@@ -10,9 +10,12 @@ package io.renren;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RenrenApplication {
 
 	public static void main(String[] args) {
