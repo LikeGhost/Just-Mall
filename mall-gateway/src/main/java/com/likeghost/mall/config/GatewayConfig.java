04c22f095284b7c8c6f1a0948fee1965aa5964fd
LikeGhost <1154083659@qq.com>
2023-04-14 21:05:08 +0800

    [新增功能](develop-spu-sku-save): 更新gateway和thirdparty的SpringBoot以及SpringCloud版本

--- mall-gateway/src/main/java/com/likeghost/mall/config/GatewayConfig.java
+++ mall-gateway/src/main/java/com/likeghost/mall/config/GatewayConfig.java
@@ -4,23 +4,26 @@ package com.likeghost.mall.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
+/**
+ * @author LikeGhost
+ * @date 2023/4/14 21:05
+ * @description
+ */
 @Configuration
 public class GatewayConfig {
 
     @Bean
    public CorsWebFilter corsWebFilter(){
        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-       CorsConfiguration corsConfiguration = new CorsConfiguration();
-       corsConfiguration.addAllowedHeader("*");
-       corsConfiguration.addAllowedMethod("*");
-       corsConfiguration.addAllowedOrigin("*");
-       corsConfiguration.setAllowCredentials(true);
-       source.registerCorsConfiguration("/**",corsConfiguration);
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOriginPattern("*");
+        corsConfiguration.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", corsConfiguration);
 
-       return new CorsWebFilter(source);
-   }
+        return new CorsWebFilter(source);
+    }
 }
